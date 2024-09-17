c9cf1dc4ef6bd3897440a119737e5d0d14729a6c
Samay Pessoa <arolllysamay@gmail.com>
2024-09-17 12:02:16 -0300

    feat: :sparkles: usecase product

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,31 @@
+import ProductFactory from "../../../domain/product/factory/product.factory"
+import UpdateProductUseCase from "./update.product.usecase"
+
+const product = ProductFactory.create("Product", 1)
+
+const input = {
+    id: product.id,
+    name: "Product updated",
+    price: 2
+}
+
+const MockRepository = () => {
+    return {
+        create: jest.fn(),
+        update: jest.fn(),
+        find: jest.fn().mockReturnValue(Promise.resolve(product)),
+        findAll: jest.fn()
+    }
+}
+
+describe("Unit test for product update use case", () => {
+
+    it("should update a product", async () => {
+
+        const productRepository = MockRepository()
+        const productUpdateUseCase = new UpdateProductUseCase(productRepository)
+        const output = await productUpdateUseCase.execute(input)
+
+        expect(output).toEqual(input)
+    })
+})
